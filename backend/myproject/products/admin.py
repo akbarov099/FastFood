1b3bdadff1efec6d40e79169c2c608c0c89327cb
Akbarali <akbarali@Pros-MacBook-Pro.local>
2024-07-28 15:47:17 +0600

    ingridents

--- backend/myproject/products/admin.py
+++ backend/myproject/products/admin.py
@@ -1,5 +1,6 @@
+# products/admin.py
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Ingredient
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,11 +9,16 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'category_name', 'created_at', 'updated_at')
-    search_fields = ('name', 'category__name')
+    list_display = ('name', 'price', 'ingredient_list', 'category_name', 'created_at', 'updated_at')
+    search_fields = ('name', 'category__name', 'ingredients__name')
     list_filter = ('category',)
 
     def category_name(self, obj):
-        return obj.category.name
+        return obj.category.name if obj.category else 'No Category'
     category_name.short_description = 'Category'
     category_name.admin_order_field = 'category__name'
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
--- backend/myproject/products/views.py
+++ backend/myproject/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -7,5 +7,10 @@ class ProductListView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
